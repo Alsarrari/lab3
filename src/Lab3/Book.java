46e4bfd6806587573250d72702f5a84842baa46d
abdulrhman.alsarari <abdulrhman.alsarari1@gmail.com>
2025-10-30 21:18:15 +0300

    Clean up Book class by removing unused code
    Removed commented-out constructor and review methods.

--- src/Lab3/Book.java
+++ src/Lab3/Book.java
@@ -5,11 +5,6 @@ import java.util.ArrayList;
 public class Book extends Media {
     private int stock;
     private ArrayList<String> reviews;
-//
-//    public Book(int stock, String review) {
-//        this.stock = stock;
-//        this.eview = review;
-//    }
 
     public Book(String title, String auteur, String ISBN, double price, int stock, String review) {
         super(title, auteur, ISBN, price);
@@ -24,15 +19,8 @@ public class Book extends Media {
     public void setStock(int stock) {
         this.stock = stock;
     }
-//
-//    public String getReview() {
-//        return reviews;
-//    }
-//
-//    public void setReview(String review) {
-//        this.reviews = review;
-//    }
+    
 
 
-
+    
 }
